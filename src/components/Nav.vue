c14a1aba0ea49e5ccc3bca427e99ee62eb8cdc26
Erico Pereira <erico_perera@hotmail.com>
2023-07-11 09:54:41 -0400

    fist-commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,26 @@
+<script setup>
+    import {RouterLink} from "vue-router"
+    import Container from "./Container.vue"
+    import { ref } from "vue";
+
+    const searchUsermane = ref("")
+
+    function onSearch() {
+        
+    }
+</script>
+
+<template>
+    <ALayoutHeader>
+        <Container>
+            <RouterLink to="/">Instagram</RouterLink>
+            <AInputSearch
+                v-model:value="searchUsermane"
+                placeholder="Username..."
+                style="width: 200px"
+                @search="onSearch"
+            />
+        </Container>
+        
+    </ALayoutHeader>
+</template>
